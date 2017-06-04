f24e95bc0cda2875a378453c85b6d25ed2967dc3
Fernando Serboncini <fserb@fserb.com.br>
2017-06-04 13:48:33 -0400

    Fetching riot static API

---
+++ fetch_riot.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+wget http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/rune.json -O riot/rune.json
+wget http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/mastery.json -O riot/mastery.json
+wget http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/item.json -O riot/item.json
